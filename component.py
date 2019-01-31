c6ffe30357fab43a39c53dd31190cc7bd88fa04f
Alelizzt <alejandrolistc@hotmail.com>
2019-01-31 16:13:22 -0500

    refactoring incidents

--- component.py
+++ component.py
@@ -22,45 +22,32 @@ headers = {
     "Content-Type": "application/json"
 }
 
-def updateStatusIncident(status, message):
+def updateStatusIncident(statusIncident, statusComponent, message):
     url = "{}/incidents".format(CACHET_API_URL)
     payload = {
+      "component_id" : "{}".format(COMPONENT_ID),
+      "component_status": statusComponent,
+      "visible": 1,
+      "status": statusIncident,
+      "message": message, 
       "name": "Error with {}".format(COMPONENT_NAME), 
-      "message": message,
-      "status": status,
-      "visible": 1
     }
 
     response = requests.post(url, headers=headers, data=json.dumps(payload))
     print("Generating Incident: ", response.text)
-
-def updateStatusComponent(status):
-    url = "{}/components/{}".format(CACHET_API_URL, COMPONENT_ID)
-    payload = {
-      "name": "{}".format(COMPONENT_NAME),
-      "description": "{}".format(COMPONENT_DESCRIPTION),
-      "status": status
-    }
-
-    response = requests.put(url, headers=headers, data=json.dumps(payload))
-    print("Update component: ", response.text)
 
 try:
   respponse = requests.get("{}".format(COMPONENT_URL))
   if respponse.status_code is 200:
     updateStatusComponent(Operational)
   else:
-    updateStatusComponent(Partial_Outage)
-    updateStatusIncident(Investigating, "Error {}".format(respponse.status_code))
+    updateStatusIncident(Investigating, Partial_Outage, "Error {}".format(respponse.status_code))
 except requests.HTTPError as error:
-  updateStatusComponent(Major_Outage)  
-  updateStatusIncident(Investigating, "HTTP error - {}".format(error))
+  updateStatusIncident(Investigating, Major_Outage, "HTTP error - {}".format(error))
 except requests.exceptions.ConnectionError as con_error:
   print("Doesn't have Internet access! {}".format(conn_error))
   updateStatusComponent(Major_Outage)
 except requests.exceptions.Timeout as time_error:
-  updateStatusComponent(Major_Outage)
-  updateStatusIncident(Investigating, "TimeOut Error {}".format(time_error))
+  updateStatusIncident(Investigating, Major_Outage, "TimeOut Error {}".format(time_error))
 except:
-  updateStatusComponent(Major_Outage)
-  updateStatusIncident(Investigating, "Unknown error [ CheckServerToResolve ]")
+  updateStatusIncident(Investigating, Major_Outage, "Unknown error [ CheckServerToResolve ]")
