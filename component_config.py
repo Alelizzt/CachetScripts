71e422e440d6e8112d2476722d8d6024b3a4a0d6
Alelizzt <alejandrolistc@hotmail.com>
2019-01-31 14:31:16 -0500

    Add component variable template

---
+++ component_config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+CACHET_TOKEN = "GET YOUR TOKEN" # Get your token at /dashboard/user
+CACHET_API_URL = "http://ADDRESS OF YOUR CACHET PAGE/api/v1" 
+
+COMPONENT_NAME = "COMPONENT" 
+COMPONENT_ID = "NUMBER" # You can see the id component on Cachet dashboard/edit component. That number in the URL it is.
+COMPONENT_DESCRIPTION = "Check if {} works".format(COMPONENT_NAME)
+COMPONENT_URL = "https://COMPONENT URL.com"
+
